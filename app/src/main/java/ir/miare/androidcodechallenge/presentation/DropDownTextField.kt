378460a95e1dcb4a4b4d1275ed5a1c9e5d7ecc11
Ali's Macbook <apple@APPLEs-MacBook-Pro.local>
2025-05-23 02:19:44 +0330

    Add DropDownTextField for sort section

---
+++ app/src/main/java/ir/miare/androidcodechallenge/presentation/DropDownTextField.kt
@@ -0,0 +1,106 @@
+package ir.miare.androidcodechallenge.presentation
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.unit.sp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DropdownTextField(
+    options: List<Pair<String, Int>>,
+    selectedOption: String,
+    onOptionSelected: (Pair<String, Int>) -> Unit,
+    label: String = "", modifier: Modifier = Modifier
+) {
+    var expanded by remember { mutableStateOf(false) }
+    CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Rtl) {
+
+        ExposedDropdownMenuBox(
+            expanded = expanded,
+            onExpandedChange = { expanded = !expanded }
+        ) {
+            androidx.compose.material3.OutlinedTextField(
+                value = selectedOption,
+                onValueChange = {},
+                readOnly = true,
+                label = {
+                    Text(
+                        text = label,
+                        style = MaterialTheme.typography.body2
+                    )
+                },
+                trailingIcon = {
+                    ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded)
+                },
+                placeholder = {
+                    Text(
+                        text = "Select Sorting",
+                        style = MaterialTheme.typography.body2
+                    )
+                },
+                modifier = modifier
+                    .menuAnchor()
+                    .fillMaxWidth(),
+                colors = TextFieldDefaults.colors(
+                    focusedIndicatorColor = Color.Blue,
+                    unfocusedIndicatorColor = Color.Gray,
+                    errorIndicatorColor = Color.Red,
+                    focusedContainerColor = Color.White,
+                    unfocusedContainerColor = Color.White,
+                    errorContainerColor = Color.White,
+                    focusedTextColor = Color.Gray,
+                    focusedPlaceholderColor = Color.Blue,
+                    unfocusedPlaceholderColor = Color.Gray,
+                    focusedLabelColor = Color.Blue,
+                    unfocusedLabelColor = Color.Gray,
+                ),
+                textStyle = MaterialTheme.typography.body2
+            )
+
+            ExposedDropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false },
+                modifier.background(color = Color.White)
+            ) {
+                options.forEach { option ->
+                    DropdownMenuItem(
+                        text = {
+                            Text(
+                                modifier = Modifier.fillMaxWidth(),
+                                text = option.first,
+                                fontSize = 16.sp,
+                                style = MaterialTheme.typography.body2,
+                                color = if (selectedOption == option.first) Color.White else Color.Gray,
+                                textAlign = TextAlign.Start
+                            )
+                        },
+                        onClick = {
+                            onOptionSelected(option)
+                            expanded = false
+                        }, modifier = modifier.background(
+                            color = if (selectedOption == option.first) Color.Blue else Color.White
+                        )
+                    )
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/ir/miare/androidcodechallenge/presentation/SortingSection.kt
+++ app/src/main/java/ir/miare/androidcodechallenge/presentation/SortingSection.kt
@@ -1,76 +1,64 @@
 package ir.miare.androidcodechallenge.presentation
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.selection.selectable
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.RadioButton
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-
 
 @Composable
 fun SortingSection(
     selectedOption: Int,
     onOptionSelected: (Int) -> Unit
 ) {
+    val options = listOf(
+        "None" to -1,
+        "Team & league ranking" to 0,
+        "Most goals scored by a player" to 1,
+        "Average goal per match in a league" to 2
+    )
+    var selectedText = options.find { it.second == selectedOption }?.first ?: "Select Sorting"
+//    var SortingValue by remember { mutableStateOf("Sorting") }
+
     Column(
         modifier = Modifier
             .fillMaxWidth()
             .background(Color.White)
             .padding(top = 16.dp, bottom = 16.dp)
     ) {
-        androidx.compose.material.Text(
+        Text(
             text = "Sorting by:",
             style = MaterialTheme.typography.body1.copy(fontWeight = FontWeight.Bold),
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(horizontal = 16.dp),
-            textAlign = androidx.compose.ui.text.style.TextAlign.Center
+            textAlign = TextAlign.Center
         )
-        Column(
+        Box(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(top = 16.dp, start = 8.dp, end = 8.dp)
         ) {
-            val options = listOf(
-                "Team & league ranking" to 0,
-                "Most goals scored by a player" to 1,
-                "Average goal per match in a league" to 2,
-                "None" to -1
+            DropdownTextField(
+                options,
+                label = "Select Sorting",
+                selectedOption = selectedText,
+                onOptionSelected = {
+                    selectedText = it.first
+                    onOptionSelected.invoke(it.second)
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 12.dp)
             )
-            options.forEach { (text, mode) ->
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .selectable(
-                            selected = (mode == selectedOption),
-                            onClick = { onOptionSelected(mode) }
-                        )
-                        .padding(vertical = 8.dp),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    RadioButton(
-                        selected = (mode == selectedOption),
-                        onClick = { onOptionSelected(mode) }
-                    )
-                    androidx.compose.material.Text(
-                        text = text,
-                        style = MaterialTheme.typography.body2.copy(
-                            fontWeight = FontWeight.Bold,
-                            color = Color.Black
-                        ),
-                        modifier = Modifier.padding(start = 8.dp)
-                    )
-                }
-            }
         }
     }
 }
